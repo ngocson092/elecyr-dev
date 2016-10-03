79b45a8f1f133958f7192101cea6b5510a9d8d99
Sonlexus <ngocson092@gmail.com>
2016-10-03 18:14:07 +0700

    edit route

--- routes/api.js
+++ routes/api.js
@@ -17,6 +17,83 @@ var data = {
   ]
 };
 
+
+
+var moment = require('moment');
+moment().format();
+
+var MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server;
+
+//mongodb://<dbuser>:<dbpassword>@ds033933-a0.mongolab.com:33933,ds033933-a1.mongolab.com:33933/<dbname>?replicaSet=rs-ds033933
+    var urldbevernote = 'mongodb://elecyrAdmin:zqpM1029@ds033933-a0.mongolab.com:33933,ds033933-a1.mongolab.com:33933/dbevernote?replicaSet=rs-ds033933'
+
+var dbArticleColl = 'articles';
+var dbTagColl = 'tags';
+
+
+
+//Fetch the blog Articles, this function makes an ajax call to database to get the content
+router.get('/fetchArticles', function(req, res){
+
+  req.logger.log('info','1: (/fetchArticles) function entry');
+
+  var fetchEntries = function(db, callback){
+
+    db.collection(dbArticleColl).find().toArray(function(err, docs){
+      req.logger.log('db','2: (/fetchArticles) docs: ' + docs);
+      callback(docs);
+    })
+  };
+
+  MongoClient.connect(urldbevernote, function(err, db){
+    fetchEntries(db, function(docs){
+
+      db.close();
+
+      res.send(docs);
+    });
+  });
+})
+
+
+//Call the 'blogs' collection from the mongoDB
+router.get('/solar-blog', function(req, res){
+
+  var getBlogTags = function(db, callback){
+
+    db.collection(dbTagColl).find({'_id':1}).toArray(function(err, docs){
+      //	console.log(docs);
+
+      var tagObject = {};
+      tagObject = docs[0].tagObject;
+      //		req.logger.log('info', "The tagObject" + JSON.stringify(tagObject, null, 4));
+      var tags = new Array();
+
+      for(var key in tagObject){
+        tags.push(tagObject[key]);
+      }
+      callback(tags);
+    })
+  };
+
+  MongoClient.connect(urldbevernote, function(err, db){
+    getBlogTags(db, function(tags){
+
+      req.logger.log('db', "1: (tags) returned from database" + JSON.stringify(tags, null, 4));
+
+      db.close();
+
+
+      res.render('solar-blog', {
+        title: "Solar Blog",
+        tags: tags
+      });
+    });
+  });
+})
+
+
 // GET
 
 exports.posts = function (req, res) {
