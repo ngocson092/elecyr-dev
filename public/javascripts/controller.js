a60a4420c1517b302aa74b322c5b9012bb362813
Sonlexus <ngocson092@gmail.com>
2016-10-05 17:54:28 +0700

    loadding post

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -2,6 +2,8 @@ app = angular.module("everBlog",['ngRoute', 'seo'])
 
 
 
+
+
 //for dynamically loading the title
 app.run(['$rootScope', '$routeParams', 'loadingService', function($rootScope, $routeParams, loadingService){
 	$rootScope.$on('$routeChangeSuccess', function(event, current, previous){
@@ -125,7 +127,11 @@ app.controller('BlogCtrl', function($scope, $http, $sce, $routeParams, $window, 
 	$scope.myFilter = function (param) {
 		return {categories:param}
 	};
-	
+
+
+	$scope.loading = true;
+
+
 	$scope.updateData = function(filter, myTempDateFilter, callback){ /*calls the function to load more blog articles */
 		//console.log("9 - updateDate function - filter: " + filter);
 		if(filter === undefined){
@@ -221,6 +227,9 @@ app.controller('BlogCtrl', function($scope, $http, $sce, $routeParams, $window, 
 				return authorStr;
 			}
 		}
+		
+		$scope.loading = false;
+		
 	}
 	if($scope.articles === undefined){
 		$scope.updateData();
@@ -292,10 +301,36 @@ app.directive('sideHeight', function(){ /* make the height of the sidebar be no 
 	}
 })
 
+app.directive('loading',   ['$http' ,function ($http)
+{
+	return {
+		restrict: 'A',
+		link: function (scope, elm, attrs)
+		{
+			scope.isLoading = function () {
+				return $http.pendingRequests.length > 0;
+			};
+
+			scope.$watch(scope.isLoading, function (v)
+			{
+				if(v){
+					elm.show();
+				}else{
+					elm.hide();
+				}
+			});
+		}
+	};
+
+}])
+
+
 /*
 routing
 */
 app.config(function($routeProvider, $locationProvider){ /* the page routing */
+
+
 	$locationProvider.html5Mode({
 		enabled:true,
 		requireBase: false,
